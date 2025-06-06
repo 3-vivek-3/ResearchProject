24b9c67f2e6978d2117be751aa76c795b3f94fe7
Vivek Kasyap Pasumarthi <V.K.Pasumarthi@student.tudelft.nl>
2025-06-06 14:15:21 +0200

    Updated settings simulator to remove the need to manually specify environment configs. All information is drawn from the data file used

--- src/utility/SettingsSimulator.py
+++ src/utility/SettingsSimulator.py
@@ -28,26 +28,30 @@ class SettingsSimulator:
         #data_file_name = "env_500x50x200_50%_sparsity.pkl.gz"
         #data_file_name = "env_500x50x200_90%_sparsity.pkl.gz"
         data_file_name = "env_500x50x200_90%_sparsity_50_trials.pkl.gz"
+        #data_file_name = "env2_500x50x200_90%_sparsity_50_trials.pkl.gz"
 
         #data_file_name = "env_100x10x20_10%_sparsity.pkl.gz"
         #data_file_name = "env_100x10x20_50%_sparsity.pkl.gz"
         #data_file_name = "env_100x10x20_90%_sparsity.pkl.gz"
 
-        data_file = Path("data") / data_file_name
-        if data_file.exists():
-            with gzip.open(data_file, "rb") as f:
+        self.data_file = Path("data") / data_file_name
+        if self.data_file.exists():
+            with gzip.open(self.data_file, "rb") as f:
                 loaded = pickle.load(f)
             self.trials_action_sets = loaded["action_sets"]
             self.trials_theta       = loaded["thetas"]
             self.trials_action_sets_recorded = True
         else:
             raise RuntimeError("Data file not loaded")
-            #self.trials_action_sets_recorded = False
-            #self.trials_action_sets = []   # list of lists of action matrices per trial
-            #self.trials_theta       = []   # list of theta vectors per trial
 
         self.logger = ResultLogger(self.name)
         self.logger.new_log()
+
+        self.trials = len(self.trials_action_sets)
+        self.horizon, self.actions, self.d = self.trials_action_sets[0].shape
+
+        print(f"\ntrials: {self.trials}, horizon: {self.horizon}, actions: {self.actions}, amb_dim: {self.d}")
+
 
         self._replicate_settings(self.logger.get_results_dir(self.filename))
         self.visualizer : Visualizer = Visualizer(self.logger.log_dir, self.do_export, self.do_show)
@@ -63,6 +67,7 @@ class SettingsSimulator:
         assert data["name"] is not None
 
         self.name = data["name"]
+        #self.do_export = data.get("export_figures", False)
         self.do_export = data["export_figures"]
         self.do_show = data["show_figures"]
 
@@ -82,15 +87,20 @@ class SettingsSimulator:
     def _replicate_settings(self, file_path : str):
 
         # Determine the total number of trials
-        total_trials = sum(map(lambda x : x["trials"], self.settings))
+        #total_trials = sum(map(lambda x : x["trials"], self.settings))
 
         data = {
             "name" : self.name,
-            "date" : datetime.now().strftime("%Y/%m/%d-%H:%M:%S"),
+            "date" : datetime.now().strftime("%d/%m/%Y-%H:%M:%S"),
+            "data file" : str(self.data_file),
 
             "number of simulations" : self.num_simulations,
-            "number of trials" : total_trials,
-            "simulation names" : self.run_names,
+            #"number of trials" : total_trials,
+            #"simulation names" : self.run_names,
+            "trials" : self.trials,
+            "horizon" : self.horizon,
+            "actions" : self.actions,
+            "ambient dimension" : self.d, 
 
             "simulations" : self.settings
         }
@@ -108,48 +118,37 @@ class SettingsSimulator:
 
         # Extract the parameters
         name = curr_settings["name"]
-        trials = curr_settings["trials"]
-        horizon = curr_settings["horizon"]
 
         env_cls = getattr(Environments, curr_settings["env"])
         learner_cls = getattr(Learners, curr_settings["learner"])
 
-
-        for trial in trange(trials, desc=f"Running {name}"):
+        for trial in trange(self.trials, desc=f"Running {name}"):
             print("\nTrial: ", trial + 1)
             # Set up the logger
             self.logger.set_simulation(name, trial + 1)
 
             # Build environment parameters, always copy base config
-            env_params = dict(curr_settings["env_config"])
+            #env_params = dict(curr_settings["env_config"])
+            env_params = {
+                "d" : self.d,
+                "actions" : self.actions
+            }
 
             if self.trials_action_sets_recorded:
                 env_params["action_sets"] = self.trials_action_sets[trial]
                 env_params["true_theta"]  = self.trials_theta[trial]
-
-            
-            #if self.curr_simulation > 0:
-            #    # Replay recorded action sets and theta after 1st simulation
-            #    env_params["action_sets"] = self.trials_action_sets[trial]
-            #    env_params["true_theta"] = self.trials_theta[trial]
-
             
             # Instantiate a new copy of the environment and learner
             env : AbstractEnvironment = env_cls(env_params)
-            learner : AbstractLearner = learner_cls(horizon, curr_settings["learner_config"])
+            learner : AbstractLearner = learner_cls(self.horizon, self.d, curr_settings["learner_config"])
 
             learner.run(env, self.logger)
 
-            print("\n")
-            print(env.get_theta())
-            print("\n")
-            print(learner.get_selected_features())
+            #print("\n")
+            #print(env.get_theta())
+            #print("\n")
+            #print(learner.get_selected_features())
 
-            #if self.curr_simulation == 0:
-            #    self.trials_action_sets.append(env.get_recorded_action_sets())
-            #    self.trials_theta.append(env.get_theta())
-
-        #self.trials_action_sets_recorded = True
         self.curr_simulation += 1
 
     def simulate_all(self):
