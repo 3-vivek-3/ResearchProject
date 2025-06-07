c4dd02b8d52f6c17fac2b790862aca98a34d89dd
Vivek Kasyap Pasumarthi <V.K.Pasumarthi@student.tudelft.nl>
2025-06-07 23:50:58 +0200

    removed unecessary simple example simulation

--- src/main.py
+++ src/main.py
@@ -18,46 +18,13 @@ def complex_simulation():
     #simulator = SettingsSimulator(settings_dir, "Egreedy_config.json")
     #simulator = SettingsSimulator(settings_dir, "Egreedy_config1.json")
     #simulator = SettingsSimulator(settings_dir, "Egreedy_config_fs.json")
-    simulator = SettingsSimulator(settings_dir, "Egreedy_config_playground.json")
+    #simulator = SettingsSimulator(settings_dir, "Egreedy_config_playground.json")
     #simulator = SettingsSimulator(settings_dir, "Egreedy_config_playground1.json")
 
-    #simulator = SettingsSimulator(settings_dir, "ETC_sparse_config.json")
+    simulator = SettingsSimulator(settings_dir, "ETC_config.json")
     #simulator = SettingsSimulator(settings_dir, "LinUCB_sparse_config.json")
     #simulator = SettingsSimulator(settings_dir, "ETC_sparse_config_test.json")
     simulator.simulate_all()
-
-def simple_example():
-    print("Beginning a Bandit Simulation")
-
-    simulation_name = "Example_Simulation"
-
-    env = LinearEnvironment({
-        "d" : 2,
-        "action_set" : list([ [x, y] for x in range(3) for y in range(4)]),
-        "true_theta" : [5, -3],
-        "sigma" : 1,
-        "k" : 12
-    })
-
-    learner = ETCLearner(24, {})
-
-    logger = ResultLogger(simulation_name)
-    logger.new_log()
-
-    learner.run(env, logger)
-    rewards = learner.cum_reward()
-    cum_regret = env.get_cumulative_regret()
-
-    print("Terminating the Bandit Simulation")
-
-    plt.title("Rewards over time")
-    plt.plot(np.cumsum(rewards), label="ETC")
-    plt.legend()
-    plt.show()
-
-    plt.title("Regret")
-    plt.plot(cum_regret)
-    plt.show()
 
 if __name__ == "__main__":
     main()
