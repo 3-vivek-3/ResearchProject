161f92bae673637d8a0ea68c2f39035d02155f6e
Vivek Kasyap Pasumarthi <V.K.Pasumarthi@student.tudelft.nl>
2025-06-24 15:30:52 +0200

    Added TSLearner to Learner init file

--- src/Learners/__init__.py
+++ src/Learners/__init__.py
@@ -3,5 +3,6 @@ from .ConcreteLearners.ETCLearner import ETCLearner
 from .ConcreteLearners.LinUCBLearner import LinUCBLearner
 from .ConcreteLearners.UCBLearner import UCBLearner
 from .ConcreteLearners.EGreedyLearner import EGreedyLearner
+from .ConcreteLearners.TSLearner import TSLearner
 
-__all__ = ["AbstractLearner", "ETCLearner", "LinUCBLearner", "UCBLearner, EGreedyLearner"]
+__all__ = ["AbstractLearner", "ETCLearner", "LinUCBLearner", "UCBLearner", "EGreedyLearner", "TSLearner"]
