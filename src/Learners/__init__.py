31e234f3c57153a56a5d7c8537ae13616e7b2942
Vivek Kasyap Pasumarthi <V.K.Pasumarthi@student.tudelft.nl>
2025-06-24 18:13:33 +0200

    Removed UCBLearner

--- src/Learners/__init__.py
+++ src/Learners/__init__.py
@@ -1,8 +1,7 @@
 from .AbstractLearner import AbstractLearner
 from .ConcreteLearners.ETCLearner import ETCLearner
 from .ConcreteLearners.LinUCBLearner import LinUCBLearner
-from .ConcreteLearners.UCBLearner import UCBLearner
 from .ConcreteLearners.EGreedyLearner import EGreedyLearner
 from .ConcreteLearners.TSLearner import TSLearner
 
-__all__ = ["AbstractLearner", "ETCLearner", "LinUCBLearner", "UCBLearner", "EGreedyLearner", "TSLearner"]
+__all__ = ["AbstractLearner", "ETCLearner", "LinUCBLearner", "EGreedyLearner", "TSLearner"]
